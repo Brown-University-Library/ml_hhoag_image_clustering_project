b1cbd00ef396f0264e1eeba83d8082f973efcf64
Birkin James Diana <birkin.diana@gmail.com>
2025-02-20 06:51:55 -0500

    gets the pids-and-modsID list.

--- gather_jpegs.py
+++ gather_jpegs.py
@@ -7,6 +7,7 @@
 
 
 import logging
+import pprint
 from typing import Any
 
 import httpx
@@ -18,21 +19,33 @@ log = logging.getLogger(__name__)
 ORG_ITEM_URL = 'https://repository.library.brown.edu/api/items/bdr:9r3a8c4a/'
 
 
-def fetch_pids(url: str) -> list[str]:
+def fetch_parts(url: str) -> list[dict[str, str]]:
+    """
+    Returns a list of dictionaries with keys 'pid' and 'mods_id_bdr_pid_ssim', like:
+    [ {'pid': 'bdr:gbzad5mb', 'mods_id_bdr_pid_ssim': 'HH018977_0001'}, {etc...} }
+    """
     with httpx.Client() as client:
         response: httpx.Response = client.get(url)
         response.raise_for_status()
         data: dict[str, Any] = response.json()
-
+    ## get the relations-->hasPart data -----------------------------
     has_parts: list[dict[str, Any]] = data.get('relations', {}).get('hasPart', [])
-    pids: list[str] = [part.get('pid') for part in has_parts if part.get('pid') is not None]
-    return pids
+    pids_and_IDs: list[dict[str, str]] = []
+    for part in has_parts:
+        ## build the pid and mods_id list ---------------------------
+        part: dict[str, Any] = part
+        pid: str | None = part.get('pid')
+        mods_id_list: list[str] = part.get('mods_id_bdr_pid_ssim', [])
+        mods_id: str | None = mods_id_list[0] if mods_id_list else None
+        if pid is not None and mods_id is not None:
+            pids_and_IDs.append({'pid': pid, 'mods_id_bdr_pid_ssim': mods_id})
+    return pids_and_IDs
 
 
 def main() -> None:
-    pids: list[str] = fetch_pids(ORG_ITEM_URL)
-    log.info(f'found ``{len(pids)}`` pids')
-    log.info(f'pids: ``{pids}``')
+    pids_and_IDs: list[dict[str, str]] = fetch_parts(ORG_ITEM_URL)
+    log.info(f'found ``{len(pids_and_IDs)}`` entries')
+    log.info(f'pids_and_IDs: ``{pprint.pformat(pids_and_IDs)}``')
 
 
 if __name__ == '__main__':
